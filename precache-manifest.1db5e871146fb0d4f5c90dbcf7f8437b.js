f7f2df21d1fc3dee56e1eb925e97bed90a1a363d
Prajakta Deotare <you@example.com>
2022-11-23 16:27:55 +0530

    Updates

---
+++ precache-manifest.1db5e871146fb0d4f5c90dbcf7f8437b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a658be9802bf16eabd7",
+    "url": "Prajaktadtr.github.io/newsmonkey/static/css/main.0d26fb4a.chunk.css"
+  },
+  {
+    "revision": "9a658be9802bf16eabd7",
+    "url": "Prajaktadtr.github.io/newsmonkey/static/js/main.9a658be9.chunk.js"
+  },
+  {
+    "revision": "9a706a69ef0a8c3185a3",
+    "url": "Prajaktadtr.github.io/newsmonkey/static/js/1.9a706a69.chunk.js"
+  },
+  {
+    "revision": "cfd6531219c3f7aaabc2",
+    "url": "Prajaktadtr.github.io/newsmonkey/static/js/2.cfd65312.chunk.js"
+  },
+  {
+    "revision": "8eee7f0db72d42f34e5c",
+    "url": "Prajaktadtr.github.io/newsmonkey/static/js/runtime~main.8eee7f0d.js"
+  },
+  {
+    "revision": "aa42f151da9798a9d3a82251b93ef656",
+    "url": "Prajaktadtr.github.io/newsmonkey/static/media/spinner.aa42f151.gif"
+  },
+  {
+    "revision": "6c26183126bd8090918626c8089403ed",
+    "url": "Prajaktadtr.github.io/newsmonkey/index.html"
+  }
+];
